631549038dae387a3df8c0a1613da5843207ec58
meggavenda-dev <meggavenda@gmail.com>
2026-01-09 08:38:24 -0300

    Add timezone utility functions to utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
 
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+DEFAULT_TZ = "America/Sao_Paulo"
+
+def get_tz(name: str | None) -> ZoneInfo:
+    return ZoneInfo(name or DEFAULT_TZ)
+
+def now_local(tz: ZoneInfo) -> datetime:
+    return datetime.now(tz)
+
